bc86ae010b87097df06585156cf9a306907ac5a9
Yoav TC <yoavtc2004@gmail.com>
2024-12-25 17:22:06 +0200

    Made MoveInformationRPC network serializable

--- Assets/_Game Assets/Scripts/Input/MoveInformationRPC.cs
+++ Assets/_Game Assets/Scripts/Input/MoveInformationRPC.cs
@@ -1,21 +1,37 @@
 ﻿using System;
+using Unity.Netcode;
 using UnityEngine;
 
 [Serializable]
-public class MoveInformationRPC
+public struct MoveInformationRPC : INetworkSerializable
 {
+    public ulong clientId;
+        
     public int circleId;
     public int circleTeam;
     
     public Vector2Int originCellCoords;
     public Vector2Int targetCellCoords;
 
-    public MoveInformationRPC(MoveInformation moveInformation)
+    public MoveInformationRPC(MoveInformation moveInformation, ulong clientId)
     {
+        this.clientId = clientId;
+        
         circleId = moveInformation.circle.id;
         circleTeam = (int) moveInformation.circle.team;
 
         originCellCoords = moveInformation.originCell.GetCoords();
         targetCellCoords = moveInformation.targetCell.GetCoords();
+    }
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref clientId);
+        
+        serializer.SerializeValue(ref circleId);
+        serializer.SerializeValue(ref circleTeam);
+        
+        serializer.SerializeValue(ref originCellCoords);
+        serializer.SerializeValue(ref targetCellCoords);
     }
 }
