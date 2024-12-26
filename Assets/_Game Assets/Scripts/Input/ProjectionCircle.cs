5ab73a33f839135aff2a4abfedf3f7abb501eb62
Yoav TC <yoavtc2004@gmail.com>
2024-12-26 16:16:48 +0200

    Improved visual of drag projection

--- Assets/_Game Assets/Scripts/Input/ProjectionCircle.cs
+++ Assets/_Game Assets/Scripts/Input/ProjectionCircle.cs
@@ -1,16 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ProjectionCircle : SnapToGridBase
 {
     [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private float lineProjectionAnimationSpeed;
     private Vector2 originalCoords;
+    private Material lineMaterial;
 
     private void Start()
     {
+        lineMaterial = lineRenderer.material;
+        
         lineRenderer.positionCount = 2;
         lineRenderer.SetPosition(0, transform.position);
 
         originalCoords = GetSnappedPosition(transform.position);
+    }
+
+    private void Update()
+    {
+        lineRenderer.material.mainTextureOffset -= new Vector2(lineProjectionAnimationSpeed * Time.deltaTime, 0f);
+        lineRenderer.material = lineMaterial;
     }
 
     public override void MoveTo(Vector3 pos)
